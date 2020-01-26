887d8c13db6ad86ac50233226634e1ae6369a836
tmampuru <romeo.mampuru@gmail.com>
2020-01-26 14:21:06 +0200

    Latest UI update

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,45 @@
-$(document).ready(function(){
-  $("#tableSch").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $("#tableItem tr").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+$(document).ready(function() {
+    $("#tableSch").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#tableItem tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
     });
-  });
 
-  $('form input').change(function () {
-    $('form p').text(this.files.length + " file(s) selected");
-  });
+    $('form input').change(function() {
+        $('form p').text(this.files.length + " file(s) selected");
+    });
 
+    // So am trying to drag and drop and say what I did drop 
+
+    $("#highlight").on('dragenter', function(ev) {
+        // Entering drop area. Highlight area
+        $("#highlight").toggleClass("highlighted-form");
+    });
+
+    $("#highlight").on('dragleave', function(ev) {
+        // Going out of drop area. Remove Highlight
+        $("#highlight").removeClass("highlighted-form");
+    });
+
+    $("#highlight").on('drop', function(ev) {
+        // Dropping files
+        ev.preventDefault();
+        ev.stopPropagation();
+        // Clear previous messages
+        $("#messages").empty();
+        if (ev.originalEvent.dataTransfer) {
+            if (ev.originalEvent.dataTransfer.files.length) {
+                var droppedFiles = ev.originalEvent.dataTransfer.files;
+                for (var i = 0; i < droppedFiles.length; i++) {
+                    console.log(droppedFiles[i]);
+                    $("#messages").append("<br /> <b>Dropped File </b>" + droppedFiles[i].name);
+                    // Upload droppedFiles[i] to server
+                }
+            }
+        }
+
+        $("#highlight").removeClass("highlighted-form");
+        return false;
+    });
 });
--- src/Controller/FileController.php
+++ src/Controller/FileController.php
@@ -17,28 +17,54 @@
   class FileController extends AbstractController
   {
     /**
-      *@Route("/khbkb")
+      *@Route("/files")
       *
     */
     public function new(Request $request)
     {
+      
       $images = new Files();
+      $finder = new Finder();
+
       $form = $this->createForm(ImageType::class, $images);
       $form->handleRequest($request);
+      
       if ($form->isSubmitted() && $form->isValid()) {
           $data = $form->getData();
-
-          $newpath = $finder->path('data');
-
-          print_r($newpath);
-
-
-
+          var_dump($data);
+          die;
         return $this->render('home/index.html.twig');
     }
       return $this->render('files/index.html.twig', [
           'form' => $form->createView(),
       ]);
+    }
+
+    /**
+      *@Route("/", name="info")
+      *
+    */
+    public function info()
+    {
+      //
+      return $this->render('info/index.html.twig');
+    }
+
+    /*
+    * @Route("info/search", name="info_search")
+    * Method({"GET", "POST"})
+    */
+
+    /**
+      *@Route("/home")
+      *
+    */
+    public function show()
+    {
+      // createfrombuilder gets form factory
+      //
+      return $this->render('home/index.html.twig');
+      
     }
   }
 ?>
